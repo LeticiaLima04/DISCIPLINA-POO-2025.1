d796c7660ba9bd7fb3ea5ef1d68b8254be0e0e4c
Leticia Lima <166670763+LeticiaLima04@users.noreply.github.com>
2025-04-05 13:51:38 -0300

    Update Principal.java

--- Lista-01/Exercícios-Propostos/Q5/src/br/edu/principal/Principal.java
+++ Lista-01/Exercícios-Propostos/Q5/src/br/edu/principal/Principal.java
@@ -7,6 +7,14 @@ public class Principal {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
+		System.out.print("Digite o preço do produto: R$ ");
+		double preco = sc.nextDouble();
+		
+		double desconto = preco * 0.10;
+		
+		double PrecoNovo = preco - desconto;
+		
+		System.out.println("O novo preço é: R$ " + PrecoNovo);
 
 	}
 
