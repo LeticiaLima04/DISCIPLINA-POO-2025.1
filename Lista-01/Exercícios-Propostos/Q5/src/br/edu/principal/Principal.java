d7047910ea3500f0118feb0a84725f37fe85f127
Leticia Lima <166670763+LeticiaLima04@users.noreply.github.com>
2025-04-05 13:50:47 -0300

    Add files via upload

---
+++ Lista-01/Exercícios-Propostos/Q5/src/br/edu/principal/Principal.java
@@ -0,0 +1,13 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+
+	}
+
+}
