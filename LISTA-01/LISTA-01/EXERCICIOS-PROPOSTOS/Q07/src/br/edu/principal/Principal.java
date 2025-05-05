f9ca380e23118d201f1d7c4f5f47c88c0f29de47
Leticia Lima <166670763+LeticiaLima04@users.noreply.github.com>
2025-05-05 10:23:30 -0300

    Add files via upload

---
+++ LISTA-01/LISTA-01/EXERCICIOS-PROPOSTOS/Q07/src/br/edu/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Digite o peso da pessoa (em kg): ");
+        double peso = scanner.nextDouble();
+
+        
+        double pesoEngordar = peso + (peso * 15 / 100);
+        double pesoEmagrecer = peso - (peso * 20 / 100);
+
+        
+        System.out.println("Se engordar 15%%, o novo peso será: ", pesoEngordar);
+        System.out.println("Se emagrecer 20%%, o novo peso será: ", pesoEmagrecer);
+
+       
+    }
+}
